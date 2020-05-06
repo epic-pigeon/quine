7593c91285cda1e011cf0e066c6804f42f4c82aa
nikita202 <nikita@ostaplyuk.com>
2020-05-07 01:32:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,1 @@
+public class Main { public static void main(String[] args) { char c = 34; System.out.println(s+c+s+c+';'+'}'); } static String s = "public class Main { public static void main(String[] args) { char c = 34; System.out.println(s+c+s+c+';'+'}'); } static String s = ";}
